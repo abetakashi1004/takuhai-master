75d67995ec3f8587f6fb0a790b1dafc9c1227d1c
abetakashi1004 <takashi.48694062@gmail.com>
2019-12-26 15:23:20 +0900

    [Edit]バッチ処理1日ごとに変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,7 +23,7 @@ env :PATH, ENV['PATH'] # 絶対パスから相対パス指定
 set :output, 'log/cron.log' # ログの出力先ファイルを設定
 set :environment, :development # 環境を設定
 
-every 1.minute do
+every 1.day do
   runner "TodayCourse.every_delete"
   runner "Takeout.every_delete"
 end
